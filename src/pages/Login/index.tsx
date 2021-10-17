7de392fafb4deef36cd556cbfd8703d2b748369d
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2021-10-17 20:30:13 -0300

    feat: imported styled components

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,15 +1,27 @@
 import drbbb1 from '../../assets/images/drbbb1.png'
+import Button from '../../components/Button'
+import Input from '../../components/Input'
+import { Container, Content, LoginDiv } from './styles'
 const Login = () => {
     return (
-        <div>
-            <h1>Faça Login</h1>
-            <div>
+        <LoginDiv>
+            <Container>
+                <Content>
+                    <h1>dribbble</h1>
+                    <h2>Discover the world’s top Designers & Creatives.</h2>
+                </Content>
                 <img src={drbbb1} alt="logo do Dribbble"/>
-            </div>
+            </Container>
             <div>
-                
+                <h3>Sign up to Dribbble</h3>
+                <p>Username os Email Address:</p>
+                <Input></Input>
+                <p>Password</p>
+                <Input></Input>
+                <Button></Button>
+
             </div>
-        </div>
+        </LoginDiv>
     )
 }
 
